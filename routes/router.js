33a083f995ba87472520000c5204628aa1609b5c
Nikita Norseev <nikita.norseev@gmail.com>
2023-06-24 01:02:03 +0300

    asdf

--- routes/router.js
+++ routes/router.js
@@ -5,5 +5,8 @@ const usersRouter = require('./users');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+router.use('/*', (req, res) => {
+  res.status(404).send({ message: 'Not Found' });
+});
 
 module.exports = router;
