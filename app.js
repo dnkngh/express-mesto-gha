57199256f093e76eeacb812963d8d07dcb989df7
Nikita Norseev <nikita.norseev@gmail.com>
2023-07-12 22:03:04 +0300

    asdf

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const helmet = require('helmet');
 const { errors } = require('celebrate');
 const router = require('./routes/router');
 
-const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errors/errorHandler');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
@@ -17,7 +16,6 @@ app.use(express.json());
 app.use(helmet());
 app.disable('x-powered-by');
 
-app.use(auth);
 app.use(router);
 app.use(errors());
 app.use(errorHandler);
--- routes/router.js
+++ routes/router.js
@@ -5,12 +5,13 @@ const usersRouter = require('./users');
 const NotFound = require('../middlewares/errors/NotFound');
 const { loginValidation, createUserValidation } = require('../middlewares/validation');
 const { login, createUser } = require('../controllers/users');
+const auth = require('../middlewares/auth');
 
 router.use('/signin', loginValidation, login);
 router.post('/signup', createUserValidation, createUser);
 
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
+router.use('/users', auth, usersRouter);
+router.use('/cards', auth, cardsRouter);
 router.use('/*', (req, res, next) => {
   next(new NotFound('Not found'));
 });
